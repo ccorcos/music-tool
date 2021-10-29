efb76903276457159763b22eb9c93c8f27b5a4f6
Chet Corcos <ccorcos@gmail.com>
2021-10-29 00:59:05 -0700

    All the different views configurable.

--- src/app/components/Squares.tsx
+++ src/app/components/Squares.tsx
@@ -15,34 +15,43 @@ const noteStyle: React.CSSProperties = {
 	margin: 0,
 }
 
-const scale = [m.root, m.minorThird, m.fourth, m.fifth, m.minorSeven]
-const startChord = [m.root, m.fifth]
-// const startChord = [m.root, m.fourth, m.minorSeven]
-// const startChord = [m.root, m.fifth, m.minorSeven]
+// Should we repeat notes in every octave?
+const REPEAT = false
+const NORMALIZE = true
 
-function incAndWrap(scale: m.Scale, note: number, amount = 1) {
-	const index = scale.indexOf(note)
-	return scale[(index + amount) % scale.length]
-}
+const scale = [m.root, m.minorThird, m.fourth, m.fifth, m.minorSeven]
+// const startChord = [m.root, m.fifth]
+// const startChord = [m.root, m.fourth, m.minorSeven]
+const startChord = [m.root, m.fifth, m.minorSeven]
 
 // Move the chords my moving note positions.
-const chords = range(scale.length * 3, scale.length * 7).map(
-	(i) => m.rotateChord(scale, startChord, i)
-	// startChord.map((note) => incAndWrap(scale, note, i))
+const chords = range(scale.length * 3, scale.length * 7).map((i) =>
+	m.rotateChord(scale, startChord, i)
 )
 
-function Row(props: { chord?: m.Scale }) {
-	const chord = props.chord || []
+function Row(props: { scale: m.Scale; chord: m.Scale }) {
+	const { chord, scale } = props
+
 	return (
 		<div style={{ display: "flex" }} onMouseEnter={() => chord.map(hit)}>
 			{range(12).map((octave) =>
-				m.diatonic.map((note) => {
-					const color = (alt: string) =>
-						chord.includes(note + octave * 12)
-							? ["#77f", "#99f", "#aaf"][chord.indexOf(note + octave * 12)]
-							: scale.includes(note)
+				m.diatonic.map((octaveNote) => {
+					// normalized view.
+					if (NORMALIZE && octaveNote + octave * 12 < chord[0] - 12) return null
+
+					const note = octaveNote + octave * 12
+
+					const color = (alt: string) => {
+						const i = chord.findIndex((n) =>
+							REPEAT ? octaveNote === n % 12 : note === n
+						)
+
+						return i !== -1
+							? ["#77f", "#99f", "#aaf"][i]
+							: scale.includes(octaveNote)
 							? "#ddd"
 							: alt
+					}
 					return (
 						<div
 							style={{
@@ -63,7 +72,7 @@ export function Squares() {
 		<div style={{ width: "100vw", height: "100vh", overflow: "auto" }}>
 			<div style={{ paddingTop: 80, paddingLeft: 40 }} onMouseDown={toggleTone}>
 				{chords.map((chord) => (
-					<Row chord={chord} />
+					<Row chord={chord} scale={scale} />
 				))}
 			</div>
 		</div>
